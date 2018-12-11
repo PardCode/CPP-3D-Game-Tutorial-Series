aebf766a11eac58b288e69d69de4693e88c943d8
PardDev <45696173+PardDev@users.noreply.github.com>
2018-12-11 15:57:22 +0100

    Window
    Code of the first video tutorial about the Window

---
+++ Tutorial1_Window/AppWindow.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Window.h"
+
+
+
+class AppWindow: public Window
+{
+public:
+	AppWindow();
+	~AppWindow();
+
+	// Inherited via Window
+	virtual void onCreate() override;
+	virtual void onUpdate() override;
+	virtual void onDestroy() override;
+};
+
---
+++ Tutorial1_Window/Window.cpp
@@ -0,0 +1,135 @@
+#include "Window.h"
+
+Window* window=nullptr;
+
+Window::Window()
+{
+	
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd,UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+	{
+		// Event fired when the window is created
+		window->onCreate();
+		break;
+	}
+
+	case WM_DESTROY:
+	{
+		// Event fired when the window is destroyed
+		window->onDestroy();
+		::PostQuitMessage(0);
+		break;
+	}
+
+
+	default:
+		return ::DefWindowProc(hwnd, msg, wparam, lparam);
+	}
+
+	return NULL;
+}
+
+
+bool Window::init()
+{
+
+
+	//Setting up WNDCLASSEX object
+	WNDCLASSEX wc;
+	wc.cbClsExtra = NULL;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.cbWndExtra = NULL;
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = NULL;
+	wc.lpszClassName = "MyWindowClass";
+	wc.lpszMenuName = "";
+	wc.style = NULL;
+	wc.lpfnWndProc = &WndProc;
+
+	if (!::RegisterClassEx(&wc)) // If the registration of class will fail, the function will return false
+		return false;
+
+	if (!window)
+		window = this;
+
+	//Creation of the window
+	m_hwnd=::CreateWindowEx(WS_EX_OVERLAPPEDWINDOW, "MyWindowClass", "DirectX Application", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1024, 768,
+		NULL, NULL, NULL, NULL);
+
+	//if the creation fail return false
+	if (!m_hwnd) 
+		return false;
+
+	//show up the window
+	::ShowWindow(m_hwnd, SW_SHOW);
+	::UpdateWindow(m_hwnd);
+
+
+	
+
+	//set this flag to true to indicate that the window is initialized and running
+	m_is_run = true;
+
+
+
+	return true;
+}
+
+bool Window::broadcast()
+{
+	MSG msg;
+
+	
+	while (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	window->onUpdate();
+
+	Sleep(1);
+
+	return true;
+}
+
+
+bool Window::release()
+{
+	//Destroy the window
+	if (!::DestroyWindow(m_hwnd))
+		return false;
+
+	return true;
+}
+
+bool Window::isRun()
+{
+	return m_is_run;
+}
+
+void Window::onCreate()
+{
+}
+
+void Window::onUpdate()
+{
+}
+
+void Window::onDestroy()
+{
+	m_is_run = false;
+}
+
+Window::~Window()
+{
+}
