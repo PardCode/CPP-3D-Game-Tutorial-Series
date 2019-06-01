2e22a8e2f21abe846299d6c0f8b2a1020b048366
PardCode <pengine3d@gmail.com>
2019-06-01 22:21:34 +0200

    Tutorial 10 - Transform Matrix
    Tutorial 10 - Transform Matrix

---
+++ Tutorial10_GraphicsEngine_TransformMatrix/AppWindow.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2019  PardCode.
+// All rights reserved.
+//
+// This file is part of CPP-3D-Game-Tutorial-Series Project, accessible from https://github.com/PardCode/CPP-3D-Game-Tutorial-Series
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License 
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+
+#pragma once
+#include "Window.h"
+#include "GraphicsEngine.h"
+#include "SwapChain.h"
+#include "DeviceContext.h"
+#include "VertexBuffer.h"
+#include "ConstantBuffer.h"
+#include "VertexShader.h"
+#include "PixelShader.h"
+
+class AppWindow: public Window
+{
+public:
+	AppWindow();
+
+	void updateQuadPosition();
+
+	~AppWindow();
+
+	// Inherited via Window
+	virtual void onCreate() override;
+	virtual void onUpdate() override;
+	virtual void onDestroy() override;
+private:
+	SwapChain * m_swap_chain;
+	VertexBuffer* m_vb;
+	VertexShader* m_vs;
+	PixelShader* m_ps;
+	ConstantBuffer* m_cb;
+private:
+	float m_old_delta;
+	float m_new_delta;
+	float m_delta_time;
+
+	float m_delta_pos;
+	float m_delta_scale;
+	float m_delta_rot;
+};
+
---
+++ Tutorial10_GraphicsEngine_TransformMatrix/Matrix4x4.h
@@ -0,0 +1,81 @@
+// Copyright (c) 2019  PardCode.
+// All rights reserved.
+//
+// This file is part of CPP-3D-Game-Tutorial-Series Project, accessible from https://github.com/PardCode/CPP-3D-Game-Tutorial-Series
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License 
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+#pragma once
+#include <memory>
+#include "Vector3D.h"
+class Matrix4x4
+{
+public:
+	Matrix4x4()
+	{
+	}
+
+	void setIdentity()
+	{
+		::memset(m_mat, 0, sizeof(float) * 16);
+		m_mat[0][0] = 1;
+		m_mat[1][1] = 1;
+		m_mat[2][2] = 1;
+		m_mat[3][3] = 1;
+	}
+
+	void setTranslation(const Vector3D& translation)
+	{
+		setIdentity();
+		m_mat[3][0] = translation.m_x;
+		m_mat[3][1] = translation.m_y;
+		m_mat[3][2] = translation.m_z;
+	}
+
+	void setScale(const Vector3D& scale)
+	{
+		setIdentity();
+		m_mat[0][0] = scale.m_x;
+		m_mat[1][1] = scale.m_y;
+		m_mat[2][2] = scale.m_z;
+	}
+
+	void operator *=(const Matrix4x4& matrix)
+	{
+		Matrix4x4 out;
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				out.m_mat[i][j] =
+					m_mat[i][0] * matrix.m_mat[0][j] + m_mat[i][1] * matrix.m_mat[1][j] + 
+					m_mat[i][2] * matrix.m_mat[2][j] + m_mat[i][3] * matrix.m_mat[3][j];
+			}
+		}
+		setMatrix(out);
+	}
+
+	void setMatrix(const Matrix4x4& matrix)
+	{
+		::memcpy(m_mat, matrix.m_mat, sizeof(float) * 16);
+	}
+
+	void setOrthoLH(float width,float height,float near_plane, float far_plane)
+	{
+		setIdentity();
+		m_mat[0][0] = 2.0f / width;
+		m_mat[1][1] = 2.0f / height;
+		m_mat[2][2] = 1.0f / (far_plane - near_plane);
+		m_mat[3][2] = -(near_plane / (far_plane - near_plane));
+	}
+
+	~Matrix4x4()
+	{
+	}
+
+public:
+	float m_mat[4][4] = {};
+};
