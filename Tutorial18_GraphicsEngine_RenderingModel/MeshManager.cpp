1e3cf3f4923903c64ce666807fbda493b662631d
PardCode <pengine3d@gmail.com>
2020-03-27 18:34:37 +0100

    Adding (c) text
    Adding (c) text

--- Tutorial18_GraphicsEngine_RenderingModel/MeshManager.cpp
+++ Tutorial18_GraphicsEngine_RenderingModel/MeshManager.cpp
@@ -1,3 +1,13 @@
+// Copyright (c) 2019 - 2020 PardCode
+// All rights reserved.
+//
+// This file is part of CPP-3D-Game-Tutorial-Series Project, accessible from https://github.com/PardCode/CPP-3D-Game-Tutorial-Series
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License 
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
 #include "MeshManager.h"
 #include "Mesh.h"
 
