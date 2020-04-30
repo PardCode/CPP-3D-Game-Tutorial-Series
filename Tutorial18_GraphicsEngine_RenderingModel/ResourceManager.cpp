f3fdd96628f1f11628acdf976c63b94334bea62b
PardCode <pengine3d@gmail.com>
2020-04-30 11:22:04 +0200

    Fixed Tutorial 17,18,19 for VS 2019
    Fixed Tutorial 17,18,19 for VS 2019

--- Tutorial18_GraphicsEngine_RenderingModel/ResourceManager.cpp
+++ Tutorial18_GraphicsEngine_RenderingModel/ResourceManager.cpp
@@ -1,5 +1,23 @@
+// Copyright (c) 2019 - 2020 PardCode
+// All rights reserved.
+//
+// This file is part of CPP-3D-Game-Tutorial-Series Project, accessible from https://github.com/PardCode/CPP-3D-Game-Tutorial-Series
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License 
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
 #include "ResourceManager.h"
+
+#if __cplusplus <= 201402L 
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#include <experimental/filesystem>
+#endif
+
+#if __cplusplus >= 201703L
 #include <filesystem>
+#endif
 
 ResourceManager::ResourceManager()
 {
@@ -12,7 +30,16 @@ ResourceManager::~ResourceManager()
 
 ResourcePtr ResourceManager::createResourceFromFile(const wchar_t * file_path)
 {
+
+#if (_MSC_VER >= 1900 && _MSC_VER <= 1916)  || ( _MSC_VER >= 1920 && __cplusplus <= 201402L) 
 	std::wstring full_path = std::experimental::filesystem::absolute(file_path);
+#endif
+
+#if _MSC_VER >= 1920 && __cplusplus > 201402L 
+	std::wstring full_path = std::filesystem::absolute(file_path);
+#endif
+
+
 
 	auto it = m_map_resources.find(full_path);
 
